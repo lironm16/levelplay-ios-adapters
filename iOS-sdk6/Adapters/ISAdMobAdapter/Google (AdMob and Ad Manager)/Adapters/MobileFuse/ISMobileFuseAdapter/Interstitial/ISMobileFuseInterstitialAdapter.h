1e7096817e781bb4aaf6e108792a9eafe0955a34
liron <lironm16@gmail.com>
2025-01-15 11:39:52 +0200

    remove dep

--- iOS-sdk6/Adapters/ISAdMobAdapter/Google (AdMob and Ad Manager)/Adapters/MobileFuse/ISMobileFuseAdapter/Interstitial/ISMobileFuseInterstitialAdapter.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "ISMobileFuseAdapter+Internal.h"
-
-@interface ISMobileFuseInterstitialAdapter : ISBaseInterstitialAdapter
-
-- (instancetype)initWithMobileFuseAdapter:(ISMobileFuseAdapter *)adapter;
-
-@end
-
--- iOS-sdk6/Adapters/ISAdMobAdapter/Google (AdMob and Ad Manager)/Adapters/Ogury/ISOguryAdapter/Interstitial/ISOguryInterstitialAdapter.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "ISOguryAdapter+Internal.h"
-#import "ISOguryAdapter+Internal.h"
-#import <IronSource/IronSource.h>
-
-@interface ISOguryInterstitialAdapter : ISBaseInterstitialAdapter
-
-- (instancetype)initWithOguryAdapter:(ISOguryAdapter *)adapter;
-@end
